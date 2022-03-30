debd16dc83258ee4e16f2da228883999d71d4a22
JackSon_tm.m <ryantokmanmokmtm@hotmail.com>
2022-03-31 01:40:20 +0800

    update

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,14 +1,14 @@
 import axios from 'axios'
 const ServerAPIUpload = (baseURL) => {
     return axios.create({
-        baseURL: baseURL || "http://ec2-3-0-57-41.ap-southeast-1.compute.amazonaws.com:8080/api/v1",
+        baseURL: baseURL || "/api/v1",
         
     })
 }
 
 const ServerAPIGet = (baseURL) => {
     return axios.create({
-        baseURL: baseURL || "http://ec2-3-0-57-41.ap-southeast-1.compute.amazonaws.com:8080/"
+        baseURL: baseURL || "/"
 })}
 
 export const apiUploadURL = (uploadData) => {
