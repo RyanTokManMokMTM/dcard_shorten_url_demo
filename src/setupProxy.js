1d05e1567891fe04787962022da61122ca66f1b6
JackSon_tm.m <ryantokmanmokmtm@hotmail.com>
2022-03-31 01:44:38 +0800

    update

--- src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function(app) {
-    app.use(createProxyMiddleware('/api/v1/**', { target: '"http://ec2-3-0-57-41.ap-southeast-1.compute.amazonaws.com:8080/"' }));
-    app.use(createProxyMiddleware('/**', { target: '"http://ec2-3-0-57-41.ap-southeast-1.compute.amazonaws.com:8080/"' }));
-};
