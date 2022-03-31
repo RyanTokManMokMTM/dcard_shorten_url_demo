f488d49876e296cbe5979d5cb2e7e27c982added
JackSon_tm.m <ryantokmanmokmtm@hotmail.com>
2022-03-31 11:49:57 +0800

    updated

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const ServerAPIUpload = (baseURL) => {
     return axios.create({
-        baseURL: baseURL || "https://ec2-3-0-57-41.ap-southeast-1.compute.amazonaws.com/api/v1",
+        baseURL: baseURL || "https://ec2-18-141-10-193.ap-southeast-1.compute.amazonaws.com/api/v1",
         headers: { 'Content-Type': 'application/json'},
     })
 }
